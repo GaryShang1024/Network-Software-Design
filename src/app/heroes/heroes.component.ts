042ddddb99fe4ed5b8c9141ad2ad0ebb52e44012
garyshang <garyshang1024@gmail.com>
2018-12-01 00:57:42 +0800

    2018/12/1

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,7 +10,7 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-  selectedHero: Hero;
+
 
   constructor(private heroService: HeroService) { }
 
@@ -21,9 +21,5 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
   }
 }
